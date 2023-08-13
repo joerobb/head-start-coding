30424dc6133a38b7169d59fb1c80892b631b94de
Joe Robb <robb_joe@hotmail.co.uk>
2023-08-13 18:36:19 +0100

    Home page updates

--- js/main.js
+++ js/main.js
@@ -27,15 +27,12 @@
 
     //Click functionality for service boxes
 
-    $(document).on("click", ".flip-container", function () {
-        $(this).toggleClass('hover');
-    });
-
     $('.service-item-container a.btn').on("click", function(event) {
         event.preventDefault();
         $(this).parent().toggleClass('active');
         $(this).siblings('.flip-container').toggleClass('hover');
     });
+
     
 
     
